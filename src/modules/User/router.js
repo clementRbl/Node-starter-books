e060b98ae063bb555fb74de2760c10d1afb9fbbc
Clément <clement.rbl@gmail.com>
2021-05-22 20:01:30 +0200

    starter books node

---
+++ src/modules/User/router.js
@@ -0,0 +1,20 @@
+class UserRouter {
+
+    constructor(router, auth, userController) {
+        this.router = router;
+        this.initializeRoutes(userController, auth);
+        return this.router;
+    }
+
+    initializeRoutes(userController, auth) {
+        this.router.route('/users')
+            .get(auth.authenticate, userController.getAll)
+            .post(userController.register);
+
+        this.router.route('/users/me')
+            .get(auth.authenticate, userController.me);
+        this.router.route('/users/authenticate').post(userController.login);
+    }
+}
+
+export default UserRouter;
